533bb42509dc24540824ca370d03ad7ed0b3927f
jxdking <33590050+jxdking@users.noreply.github.com>
2017-11-18 11:26:39 -0500

    merge

--- CachedFunc/CachedFuncSvc.cs
+++ CachedFunc/CachedFuncSvc.cs
@@ -1,106 +1,19 @@
-﻿using MagicEastern.CachedFuncBase;
+﻿using MagicEastern.CachedFunc.Base;
 using System;
 using System.Collections.Concurrent;
 using System.Runtime.Caching;
 using System.Threading;
 
-namespace MagicEastern.CachedFunc
+namespace MagicEastern.CachedFunc.Net45
 {
     public class CachedFuncSvc : CachedFuncSvcBase
     {
-        #region with cache policy, use MemoryCache as cache
-        private static int _funcID = 0;
-
-        public CachedFunc<TResult> Create<TResult>(Func<TResult> func, Func<CacheItemPolicy> policyFactory)
+        protected override ICacheHolder<TKey, TValue> GetCacheHolder<TKey, TValue>(CachedFuncOptions options)
         {
-            CachedFunc<string, TResult> cachedFunc = Create(AddInputToFunc<string, TResult>(func), policyFactory);
-            CachedFunc<TResult> ret = (fallback, nocache) => cachedFunc("", AddInputToFunc<string, TResult>(fallback), nocache);
-            return ret;
-        }
-
-        public CachedFunc<string, TResult> Create<TResult>(Func<string, TResult> func, Func<CacheItemPolicy> policyFactory)
-        {
-            var cache = MemoryCache.Default;
-            CachedFunc<string, TResult> ret = CreateFunc(cache, Interlocked.Increment(ref _funcID), func, policyFactory, PassThrough);
-            return ret;
-        }
-
-        public CachedFunc<T, TResult> Create<T, TResult>(
-            Func<T, TResult> func,
-            Func<CacheItemPolicy> policyFactory,
-            Func<T, string> keySelector)
-        {
-            if (keySelector == null) { throw new ArgumentNullException("keySelector"); }
-            var cache = MemoryCache.Default;
-            CachedFunc<T, TResult> ret = CreateFunc(cache, Interlocked.Increment(ref _funcID), func, policyFactory, keySelector);
-            return ret;
-        }
-
-        private CachedFunc<T, TResult> CreateFunc<T, TResult>(
-            MemoryCache cache,
-            int funcID,
-            Func<T, TResult> func,
-            Func<CacheItemPolicy> policyFactory,
-            Func<T, string> keySelector)
-        {
-            ConcurrentDictionary<string, object> locks = new ConcurrentDictionary<string, object>();
-            CachedFunc<T, TResult> ret = (input, fallback, nocache) =>
-            {
-                string key = keySelector(input);
-                string cacheKey = "CachedFunc" + _funcID.ToString() + keySelector(input);
-                if (key != null)
-                {
-                    if (!nocache)
-                    {
-                        object obj = cache[cacheKey];
-                        if (obj != null)
-                        {
-                            return (TResult)obj;
-                        }
-                    }
-                }
-                else
-                {
-                    throw new ArgumentNullException("[input] of the function is null or [keySelector(T)] returns null.");
-                }
-
-                object lockObj = new object();
-                lockObj = locks.GetOrAdd(key, lockObj);
-                Monitor.Enter(lockObj);
-                try
-                {
-                    object obj = cache[cacheKey];
-                    if (obj != null)
-                    {
-                        return (TResult)obj;
-                    }
-
-                    var fun = fallback ?? func;
-                    if (fun != null)
-                    {
-                        TResult res = fun(input);
-                        cache.Set(cacheKey, res, policyFactory());
-                        return res;
-                    }
-                    throw new ArgumentNullException("Please provide a [fallback] function for calculating the value. ");
-                }
-                finally
-                {
-                    locks.TryRemove(key, out object o);
-                    Monitor.Exit(lockObj);
-                }
-            };
-            return ret;
-        }
-        #endregion
-
-        private Func<T, TResult> AddInputToFunc<T, TResult>(Func<TResult> func)
-        {
-            if (func == null)
-            {
-                return null;
+            if (options != null) {
+                return new MemoryCacheHolder<TKey, TValue>(MemoryCache.Default, options); 
             }
-            return (input) => func();
+            return base.GetCacheHolder<TKey, TValue>(null);
         }
     }
 }
---
+++ CachedFuncCore/MemoryCacheHolder.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using MagicEastern.CachedFunc.Base;
+
+namespace MagicEastern.CachedFunc.Core
+{
+    class MemoryCacheHolder<TKey, TValue> : ICacheHolder<TKey, TValue>
+    {
+        private IMemoryCache _cache;
+        private MemoryCacheEntryOptions _options;
+
+        public MemoryCacheHolder(IMemoryCache cache, CachedFuncOptions options)
+        {
+            _cache = cache;
+            _options = new MemoryCacheEntryOptions
+            {
+                AbsoluteExpiration = options.AbsoluteExpiration,
+                AbsoluteExpirationRelativeToNow = options.AbsoluteExpirationRelativeToNow,
+                SlidingExpiration = options.SlidingExpiration
+            };
+        }
+
+        public bool TryGetValue(TKey key, int funcID, out TValue val) {
+            return _cache.TryGetValue<TValue>(new CacheKey<TKey>(funcID, key), out val);
+        }
+
+        public void Add(TKey key, int funcID, TValue val) {
+            _cache.Set<TValue>(new CacheKey<TKey>(funcID, key), val, _options);
+        }
+    }
+}
